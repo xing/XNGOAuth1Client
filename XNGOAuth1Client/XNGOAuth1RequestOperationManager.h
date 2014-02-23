9e24e79f4ce8a5207a8a4c664e10c3a4b7c95fd3
Piet Brauer <piet.brauer@xing.com>
2014-02-23 21:10:00 +0100

    Add OAuth methods

--- XNGOAuth1Client/XNGOAuth1RequestOperationManager.h
+++ XNGOAuth1Client/XNGOAuth1RequestOperationManager.h
@@ -1,6 +1,9 @@
 #import <AFNetworking/AFHTTPRequestOperationManager.h>
 
 @class XNGOAuth1RequestSerializer;
+@class XNGOAuthToken;
+
+FOUNDATION_EXPORT NSString *const XNGOAuth1ErrorDomain;
 
 @interface XNGOAuth1RequestOperationManager : AFHTTPRequestOperationManager
 
@@ -10,5 +13,22 @@
 - (id)initWithBaseURL:(NSURL *)baseURL consumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret;
 
 - (BOOL)deauthorize;
+
+- (void)authorizeUsingOAuthWithRequestTokenPath:(NSString *)requestTokenPath
+                          userAuthorizationPath:(NSString *)userAuthorizationPath
+                                    callbackURL:(NSURL *)callbackURL
+                                accessTokenPath:(NSString *)accessTokenPath
+                                   accessMethod:(NSString *)accessMethod
+                                          scope:(NSString *)scope
+                                        success:(void (^)(XNGOAuthToken *oAuthToken, id responseObject))success
+                                        failure:(void (^)(NSError *error))failure;
+
+- (void)acquireOAuthRequestTokenWithPath:(NSString *)path
+                             callbackURL:(NSURL *)callbackURL
+                            accessMethod:(NSString *)accessMethod
+                            requestToken:(XNGOAuthToken *)requestToken
+                                   scope:(NSString *)scope
+                                 success:(void (^)(XNGOAuthToken *requestToken, id responseObject))success
+                                 failure:(void (^)(NSError *error))failure;
 
 @end
--- XNGOAuth1Client/XNGOAuth1RequestOperationManager.m
+++ XNGOAuth1Client/XNGOAuth1RequestOperationManager.m
@@ -2,6 +2,8 @@
 #import "XNGOAuth1RequestSerializer.h"
 #import "XNGOAuthToken.h"
 
+NSString * const XNGOAuth1ErrorDomain = @"XNGOAuth1ErrorDomain";
+
 @implementation XNGOAuth1RequestOperationManager
 
 - (id)initWithBaseURL:(NSURL *)baseURL consumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret {
@@ -22,6 +24,102 @@
 
 - (BOOL)deauthorize {
     return [self.requestSerializer removeAccessToken];
+}
+
+- (void)authorizeUsingOAuthWithRequestTokenPath:(NSString *)requestTokenPath
+                          userAuthorizationPath:(NSString *)userAuthorizationPath
+                                    callbackURL:(NSURL *)callbackURL
+                                accessTokenPath:(NSString *)accessTokenPath
+                                   accessMethod:(NSString *)accessMethod
+                                          scope:(NSString *)scope
+                                        success:(void (^)(XNGOAuthToken *oAuthToken, id responseObject))success
+                                        failure:(void (^)(NSError *error))failure {
+    self.requestSerializer.requestToken = nil;
+
+    AFHTTPResponseSerializer *defaultSerializer = self.responseSerializer;
+    self.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"oauth_callback"] = [callbackURL absoluteString];
+    if (scope && !self.requestSerializer.accessToken)
+        parameters[@"scope"] = scope;
+
+    NSString *URLString = [[NSURL URLWithString:requestTokenPath relativeToURL:self.baseURL] absoluteString];
+    NSError *error;
+    NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:accessMethod URLString:URLString parameters:parameters error:&error];
+
+    if (error)
+    {
+        failure(error);
+        return;
+    }
+
+    AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        self.responseSerializer = defaultSerializer;
+        XNGOAuthToken *requestToken = [[XNGOAuthToken alloc] initWithQueryString:operation.responseString];
+        self.requestSerializer.requestToken = requestToken;
+        if (success) {
+            success(requestToken, operation.responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        self.responseSerializer = defaultSerializer;
+        if (failure) {
+            failure(error);
+        }
+    }];
+
+    [self.operationQueue addOperation:operation];
+}
+
+- (void)acquireOAuthRequestTokenWithPath:(NSString *)path
+                             callbackURL:(NSURL *)callbackURL
+                            accessMethod:(NSString *)accessMethod
+                            requestToken:(XNGOAuthToken *)requestToken
+                                   scope:(NSString *)scope
+                                 success:(void (^)(XNGOAuthToken *requestToken, id responseObject))success
+                                 failure:(void (^)(NSError *error))failure {
+
+    if (requestToken.token && requestToken.verifier) {
+        AFHTTPResponseSerializer *defaultSerializer = self.responseSerializer;
+        self.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+        NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+        parameters[@"oauth_token"]    = requestToken.token;
+        parameters[@"oauth_verifier"] = requestToken.verifier;
+
+        NSString *URLString = [[NSURL URLWithString:path relativeToURL:self.baseURL] absoluteString];
+        NSError *error;
+        NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:accessMethod URLString:URLString parameters:parameters error:&error];
+
+        if (error) {
+            failure(error);
+            return;
+        }
+
+        AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            self.responseSerializer = defaultSerializer;
+            self.requestSerializer.requestToken = nil;
+            XNGOAuthToken *accessToken = [[XNGOAuthToken alloc] initWithQueryString:operation.responseString];
+            [self.requestSerializer saveAccessToken:accessToken];
+            if (success) {
+                success(accessToken, operation.responseObject);
+            }
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            self.responseSerializer = defaultSerializer;
+            self.requestSerializer.requestToken = nil;
+            if (failure) {
+                failure(error);
+            }
+        }];
+
+        [self.operationQueue addOperation:operation];
+    }
+    else {
+        NSError *error = [[NSError alloc] initWithDomain:XNGOAuth1ErrorDomain
+                                                    code:NSURLErrorBadServerResponse
+                                                userInfo:@{NSLocalizedFailureReasonErrorKey:@"Invalid OAuth response received from server."}];
+        failure(error);
+    }
 }
 
 @end
