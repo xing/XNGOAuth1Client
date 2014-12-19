3c225773268e224ea624384d82bf32c41241e2e6
Piet Brauer <piet@nerdishbynature.com>
2014-12-19 15:05:37 +0100

    Revert "Remove destination option"
    This reverts commit 843a26201e2f95fe23eaae8fd16eaed170919cf9.
    
    Conflicts:
    	script/test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,1 +1,1 @@
-xcodebuild -workspace XNGOAuth1Client.xcworkspace -scheme 'XNGOAuth1Client' clean build test -sdk iphonesimulator$OS | xcpretty -tc; exit ${PIPESTATUS[0]}
+xcodebuild -workspace XNGOAuth1Client.xcworkspace -scheme 'XNGOAuth1Client' -destination name='iPhone 6' clean build test -sdk iphonesimulator | xcpretty -tc; exit ${PIPESTATUS[0]}
